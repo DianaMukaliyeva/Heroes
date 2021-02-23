cc76973821bbbeecaa8bec17f47a078e5194fc96
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2021-02-24 00:33:02 +0200

    Implement hero roles, and seed the heroes with roles

--- src/server/entities/hero.ts
+++ src/server/entities/hero.ts
@@ -3,37 +3,42 @@ import { ObjectType, ID, Field } from 'type-graphql';
 import { Lazy } from '../helpers';
 import { Skill } from '../entities/skill';
 import { Attribute } from './attribute';
+import { Role } from '../types/role';
 
 @Entity()
 @ObjectType()
 export class Hero {
-	@Field((type) => ID)
-	@PrimaryGeneratedColumn()
-	id!: number;
+  @Field((type) => ID)
+  @PrimaryGeneratedColumn()
+  id!: number;
 
-	@Field()
-	@Column()
-	name: string;
+  @Field()
+  @Column()
+  name: string;
 
-	@Field()
-	@Column()
-	imgUrl: string;
+  @Field()
+  @Column()
+  role: Role;
 
-	@Field()
-	@Column()
-	description: string;
+  @Field()
+  @Column()
+  imgUrl: string;
 
-	@Field()
-	@Column()
-	backStory: string;
+  @Field()
+  @Column()
+  description: string;
 
-	@Field((type) => [Skill])
-	@ManyToMany((type) => Skill, { lazy: true, cascade: ['insert'] })
-	@JoinTable()
-	skills: Lazy<Skill[]>;
+  @Field()
+  @Column()
+  backStory: string;
 
-	@Field((type) => Attribute)
-	@OneToOne((type) => Attribute, { lazy: true, cascade: ['insert'] })
-	@JoinColumn()
-	attributes: Lazy<Attribute>;
+  @Field((type) => [Skill])
+  @ManyToMany((type) => Skill, { lazy: true, cascade: ['insert'] })
+  @JoinTable()
+  skills: Lazy<Skill[]>;
+
+  @Field((type) => Attribute)
+  @OneToOne((type) => Attribute, { lazy: true, cascade: ['insert'] })
+  @JoinColumn()
+  attributes: Lazy<Attribute>;
 }
--- src/server/helpers.ts
+++ src/server/helpers.ts
@@ -4,7 +4,8 @@ import { Vault } from './entities/vault';
 import { TElement } from './types/element';
 
 const PORCU: DeepPartial<Hero> = {
-	name: 'Porcu',
+  name: 'Porcu',
+  role: 'HERO',
 	imgUrl: 'http://localhost:8080/public/porcu.png',
 	description: `
     Using his wit and engineering skills, Porcu has created an awesome suit which shoots Plasma lazers and uses immense strength to overcome his opponents. He is pretty slow though and his suit has weaknesses.
@@ -44,7 +45,8 @@ const PORCU: DeepPartial<Hero> = {
 };
 
 const LISA: DeepPartial<Hero> = {
-	name: 'Lisa McAllister',
+  name: 'Lisa McAllister',
+  role: 'SUPERHERO',
 	imgUrl: 'http://localhost:8080/public/lisa.png',
 	description: `
     With her beloved revolver called React and super-human precision, Lisa can shoot a target from a mile away. She also carries a stack of dynamites and a lasso.
@@ -88,7 +90,8 @@ const LISA: DeepPartial<Hero> = {
 };
 
 const GIDEON: DeepPartial<Hero> = {
-	name: 'Gideon',
+  name: 'Gideon',
+  role: 'SUPERHERO',
 	imgUrl: 'http://localhost:8080/public/gideon.png',
 	description: `
     Laptop that he carries with him amplifies and channels his already enormous powers. Even though he might look soft and calm, there is raging power swirling inside him. One should not get in his way.
@@ -133,8 +136,8 @@ export const seedDatabase = async () => {
 	const heroes = heroRepository.create([PORCU, LISA, GIDEON]);
 	await heroRepository.save(heroes);
 
-	const vault = vaultRepository.create({ 
-		treasures: 'This should be behind authentication.  ✧･ﾟ: *✧･ﾟ:* Loads of gold.　 *:･ﾟ✧*:･ﾟ✧' 
+	const vault = vaultRepository.create({
+		treasures: 'This should be behind authentication.  ✧･ﾟ: *✧･ﾟ:* Loads of gold.　 *:･ﾟ✧*:･ﾟ✧'
 	});
 	await vaultRepository.save(vault);
 };
