e6cd2d7a1d877aae5bf371e42795ec1da78ae5dc
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2021-02-24 23:15:50 +0200

    add flex box card container

--- src/app/views/HeroIndex/index.tsx
+++ src/app/views/HeroIndex/index.tsx
@@ -10,43 +10,56 @@ import { Footer } from '../../components/Footer';
 import { HeroCard } from '../../components/HeroCard';
 
 const HEROES_QUERY = gql`
-	query {
-		heroes {
-			name
-			imgUrl
-			description
-			backStory
-			attributes {
-				strength
-				intelligence
-				stamina
-				healthpoints
-				mana
-				agility
-				speed
-				resistance
-				weakness
-			}
-			skills {
-				name
-				damage
-				element
-			}
-		}
-	}
+  query {
+    heroes {
+      name
+      imgUrl
+      description
+      backStory
+      attributes {
+        strength
+        intelligence
+        stamina
+        healthpoints
+        mana
+        agility
+        speed
+        resistance
+        weakness
+      }
+      skills {
+        name
+        damage
+        element
+      }
+    }
+  }
 `;
 
 interface IHeroIndexProps {}
 
 const HeroCardContainer = styled.div`
-	display: flex;
-	padding: 50px;
-	align-self: center;
-	max-width: 1150px;
-	@media (min-width: 1400px) {
-		margin-left: auto;
-		margin-right: auto;
-	}
+  display: flex;
+  padding: 50px;
+  align-self: center;
+  max-width: 1150px;
+  @media (min-width: 1400px) {
+    margin-left: auto;
+    margin-right: auto;
+  }
+`;
+
+interface Props {
+  size: number;
+}
+
+const Grid = styled.div``;
+
+const Row = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 100%;
 `;
 
 const handleLoading = () => <div>Loading...</div>;
@@ -54,39 +67,41 @@ const handleLoading = () => <div>Loading...</div>;
 const handleError = (message: string) => <div>Error! {message}</div>;
 
 export const HeroIndex: React.FC<IHeroIndexProps> = () => {
-	const { data, error, loading } = useQuery(HEROES_QUERY);
+  const { data, error, loading } = useQuery(HEROES_QUERY);
 
-	if (error) {
-		return handleError(error.message);
-	}
+  if (error) {
+    return handleError(error.message);
+  }
 
-	if (loading) {
-		return handleLoading();
-	}
+  if (loading) {
+    return handleLoading();
+  }
 
-	return (
-		<main>
-			<TopBar />
-			<Hero />
-			<Section
-				heading={'Hunter Index'}
-				paragraph={`
-          Professor Hoax gave us this Hunter Index -tool 
-          so we can see how our heroes manage against evildoers. 
-          Unfortunately he forgot to implement their HeroCards. 
+  return (
+    <main>
+      <TopBar />
+      <Hero />
+      <Section
+        heading={'Hunter Index'}
+        paragraph={`
+          Professor Hoax gave us this Hunter Index -tool
+          so we can see how our heroes manage against evildoers.
+          Unfortunately he forgot to implement their HeroCards.
           It's your job to finish his work before we can continue
           on our journey together!
         `}
-			/>
+      />
 
-			{/** Improve this section. Data provided is defined on top in GraphQL query. You can decide what you use and what you dont't.*/}
-			<HeroCardContainer>
-				{data.heroes.map((hero) => (
-					<HeroCard key={hero.name} {...hero} />
-				))}
-			</HeroCardContainer>
+      {/** Improve this section. Data provided is defined on top in GraphQL query. You can decide what you use and what you dont't.*/}
+      <HeroCardContainer>
+        <Row>
+          {data.heroes.map((hero) => (
+            <HeroCard key={hero.name} {...hero} />
+          ))}
+        </Row>
+      </HeroCardContainer>
 
-			<Footer />
-		</main>
-	);
+      <Footer />
+    </main>
+  );
 };
